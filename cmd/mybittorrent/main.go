b34309bca93de9150fd51e6ce742281656ab2c87
realHugn <nothung209@gmail.com>
2024-10-10 15:39:42 +0700

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"unicode"
 	// Available if you need it!
@@ -64,6 +65,57 @@ func benDecodeInt(s string) (int64, int, error) {
 	return num, endIndex + 1, nil
 }
 
+func decodeDict(s string) (map[string]interface{}, int, error) {
+	dict := make(map[string]interface{})
+	i := 1
+	var keys []string
+
+	for i < len(s) && s[i] != 'e' {
+		key, n, err := benDecodeString(s[i:])
+		if err != nil {
+			return nil, 0, fmt.Errorf("invalid dictionary key: %v", err)
+		}
+		i += n
+
+		value, n, err := decodeBencode(s[i:])
+		if err != nil {
+			return nil, 0, fmt.Errorf("invalid dictionary value: %v", err)
+		}
+		i += n
+
+		dict[key] = value
+		keys = append(keys, key)
+	}
+
+	if i >= len(s) || s[i] != 'e' {
+		return nil, 0, fmt.Errorf("invalid dictionary: no ending 'e'")
+	}
+
+	// Check if keys are sorted
+	if !sort.StringsAreSorted(keys) {
+		return nil, 0, fmt.Errorf("invalid dictionary: keys are not sorted")
+	}
+
+	return dict, i + 1, nil
+}
+
+func benDecodeList(s string) ([]interface{}, int, error) {
+	var list = make([]interface{}, 0)
+	i := 1
+	for i < len(s) && s[i] != 'e' {
+		item, n, err := decodeBencode(s[i:])
+		if err != nil {
+			return nil, 0, err
+		}
+		list = append(list, item)
+		i += n
+	}
+	if i >= len(s) || s[i] != 'e' {
+		return nil, 0, fmt.Errorf("invalid list: no ending 'e'")
+	}
+	return list, i + 1, nil
+}
+
 func decodeBencode(s string) (interface{}, int, error) {
 	if len(s) == 0 {
 		return nil, 0, fmt.Errorf("empty input")
@@ -71,26 +123,13 @@ func decodeBencode(s string) (interface{}, int, error) {
 
 	switch {
 	case unicode.IsDigit(rune(s[0])):
-		str, n, err := benDecodeString(s)
-		return str, n, err
+		return benDecodeString(s)
 	case s[0] == 'i':
-		num, n, err := benDecodeInt(s)
-		return num, n, err
+		return benDecodeInt(s)
 	case s[0] == 'l':
-		var list = make([]interface{}, 0)
-		i := 1
-		for i < len(s) && s[i] != 'e' {
-			item, n, err := decodeBencode(s[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			list = append(list, item)
-			i += n
-		}
-		if i >= len(s) || s[i] != 'e' {
-			return nil, 0, fmt.Errorf("invalid list: no ending 'e'")
-		}
-		return list, i + 1, nil
+		return benDecodeList(s)
+	case s[0] == 'd':
+		return decodeDict(s)
 	default:
 		return nil, 0, fmt.Errorf("unsupported bencode type")
 	}
